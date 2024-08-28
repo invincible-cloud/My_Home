de7080b6fd833a9384276ffc8cccb6ebf6c7a8b4
invincible-cloud <Dingyuxin12@qq.com>
2024-08-28 22:17:19 +0800

    测试

---
+++ test.c
@@ -0,0 +1,18 @@
+/*************************************************************************
+	> File Name: test.c
+	> Author: dingxin 
+	> Mail: dingyuxin12@qq.com
+	> Created Time: Wed 28 Aug 2024 09:39:28 PM CST
+ ************************************************************************/
+
+#include<stdio.h>
+
+int main(){
+    int a,b;
+    scanf("%d %d", &a,&b);
+    int c = a+b;
+    printf("c=%d\n",c);
+    return 0;
+}
+
+
